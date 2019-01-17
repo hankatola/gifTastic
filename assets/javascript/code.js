571860ab2b8b0a206d3f80292c761cf9d1976dfc
dPaq <dpaquette2.0@gmail.com>
2019-01-17 18:27:27 -0500

    draws buttons

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
-    let animals = ['dog','puppy','cat','kitten','']
+    let animals = ['dog','puppy','cat','kitten','bird','rabbit','squirrel','shark','hamster','ferret','sugar glider','chinchilla','skunk']
 
+    function drawButtons() {
+        for (let i in animals) {
+            let btn = $('<div>').text(animals[i]).attr('data-animal',animals[i])
+            btn.addClass('btn btn-primary').appendTo($('#buttons'))
+        }
+    }
+
+    drawButtons()
 })
