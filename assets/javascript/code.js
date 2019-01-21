afa273cc4335f9f5c4e5402741a30cd6feb35c86
dPaq <dpaquette2.0@gmail.com>
2019-01-20 19:55:19 -0500

    mostly done

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
         for (let i in animals) {
             let btn = $('<div>').text(animals[i]).attr('data-animal',animals[i])
             btn.addClass('btn btn-primary').appendTo($('#buttons'))
-            btn.on('click',getGif)
         }
     }
 
@@ -18,20 +17,39 @@ $(document).ready(function() {
     }
 
     function getGif() {
+        $('#images').empty()
         let target = $(this).attr('data-animal')
         let key = '&api_key=ykFsBmZdBFsAC5DL9JfMNh01AZK5WaJM'
         let limit = '&limit=10'
+        let rating = '&rating=r'
         let url = "https://api.giphy.com/v1/gifs/search?q=" + target + key + limit
-
-
         $.get(url)
         .then(function(r){
-            console.log(r)
+            for (let i = 0; i < 10; i++) {
+                let movie = r.data[i].images.fixed_height.url
+                let still = r.data[i].images.fixed_height_still.url
+                let img = $('<img>').attr('src',still)
+                img.attr('data-still',still).attr('data-movie',movie)
+                img.attr('data-state','still')
+                img.prependTo($('#images'))
+            }
         })
-        console.log(target)
     }
 
-    $(document).on('click','#btn',addAnimal)
+    function changeURL() {
+        let state = $(this).attr('data-state'), url
+        if (state === 'still') {
+            state = 'movie'
+            url = $(this).attr('data-movie')
+        } else {
+            state = 'still'
+            url = $(this).attr('data-still')
+        }
+        $(this).attr('src',url).attr('data-state',state)
+    }
+
+    $(document).on('click','#animal-btn',addAnimal)
     $(document).on('click','.btn',getGif)
+    $(document).on('click','img',changeURL)
     drawButtons()
 })
